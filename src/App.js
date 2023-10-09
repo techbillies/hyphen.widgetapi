a5ae0b3695434922b6597e7f10e7f2f3fc5fd858
Vishnu Raja <323993+techbillies@users.noreply.github.com>
2023-10-09 23:12:23 +0530

    CSS

--- src/App.js
+++ src/App.js
@@ -6,18 +6,61 @@ import IFrame from './IFrame'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-          <header className="App-header">
-          </header>
-
-          <IFrame
-              src="https://arabianpost-dubai-news.on.drv.tw/assets.hyphen.architectured.site/iFrame.html"
-              height="100%"
-              width="100%"
-          />
-
+      <main>
+  <section className="cards">
+    <div className="card">
+      <div className="card-image">
+        <img src="https://assets.codepen.io/4787486/books-mini.jpg" />
       </div>
+      <div className="card-text">
+        <h2>Bookmaker</h2>
+        <p>
+          Space, the final frontier. These are the voyages of the Starship
+          Enterprise. Its five-year mission: to explore strange new worlds, to
+          seek out new life and new civilizations, to boldly go where no man has
+          gone before.
+        </p>
+      </div>
+      <div className="card-inner">
+        <button>Read More</button>
+        <i className="fa fa-thumbs-o-up fa-lg" aria-hidden="true" />
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-image">
+        <img src="https://assets.codepen.io/4787486/calligraphy-dragon.jpg" />
+      </div>
+      <div className="card-text">
+        <h2>Calligrapher</h2>
+        <p>
+          Nori grape silver beet broccoli kombu beet greens fava bean potato
+          quandong celery.
+        </p>
+      </div>
+      <div className="card-inner">
+        <button>Read More</button>
+        <i className="fa fa-thumbs-o-up fa-lg" aria-hidden="true" />
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-image">
+        <img src="https://assets.codepen.io/4787486/calligraphy-wits.jpg" />
+      </div>
+      <div className="card-text">
+        <h2>Famous Quotes</h2>
+        <p>
+          Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat
+          tbh semiotics artisan synth stumptown gastropub cornhole celiac swag.
+        </p>
+      </div>
+      <div className="card-inner">
+        <button>Read More</button>
+        <i className="fa fa-thumbs-o-up fa-lg" aria-hidden="true" />
+      </div>
+    </div>
+  </section>
+</main>
+
     );
   }
 }
