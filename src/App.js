313b5a74c499e05b93cf3b0f2b4da63f3a48ba27
Vishnu Raja <323993+techbillies@users.noreply.github.com>
2023-10-09 23:33:00 +0530

    Major changes

--- src/App.js
+++ src/App.js
@@ -6,60 +6,90 @@ import IFrame from './IFrame'
 class App extends Component {
   render() {
     return (
-      <main>
-  <section className="cards">
-    <div className="card">
-      <div className="card-image">
-        <img src="https://assets.codepen.io/4787486/books-mini.jpg" />
-      </div>
-      <div className="card-text">
-        <h2>Bookmaker</h2>
-        <p>
-          Space, the final frontier. These are the voyages of the Starship
-          Enterprise. Its five-year mission: to explore strange new worlds, to
-          seek out new life and new civilizations, to boldly go where no man has
-          gone before.
-        </p>
-      </div>
-      <div className="card-inner">
-        <button>Read More</button>
-        <i className="fa fa-thumbs-o-up fa-lg" aria-hidden="true" />
+     
+     <>
+  <div className="preloader">Loading...</div>
+  <div
+    style={{
+      maxWidth: 500,
+      margin: "0 auto",
+      textAlign: "center",
+      width: "100%",
+      position: "absolute",
+      left: "50%",
+      marginLeft: "-250px",
+      top: 24
+    }}
+  >
+    <a href="https://1kochi.com" title="1Kochi.com Home">
+      <img
+        src="https://cms.1arabia.com/1kochi-logo-main.png?34d534"
+        title="1Kochi - Unveiling Kochi's Essence"
+        className="k1-logo"
+        style={{ height: "auto", maxWidth: 420, width: "100%" }}
+      />
+    </a>
+  </div>
+  {/* <input type="text" id="search-input" placeholder="Search...">*/}
+  <select id="category-filter" className="custom-dropdown">
+    <option value="all"> /All</option>
+    {/* Add more options based on extracted categories */}
+  </select>{" "}
+  <div className="content-item" data-category="Local">
+    <div>
+      <div className="wrapper">
+        <div
+          className="kads1"
+          style={{
+            float: "left",
+            position: "absolute",
+            left: "50%",
+            marginLeft: "-364px",
+            top: 166,
+            maxWidth: 728,
+            width: "100%",
+            height: 90,
+            border: "1px solid #bcbcbc69",
+            textAlign: "center",
+            lineHeight: "5.59",
+            fontFamily: "arial",
+            color: "#799bc6"
+          }}
+        >
+          Advertise
+        </div>
+        <div className="panel">
+          <h2 className="city" id="city" />
+          <div className="weather" id="weather">
+            <div className="group secondary">
+              <h3 id="dt" />
+              <h3 id="description" />
+            </div>
+            <div className="group secondary">
+              <h3 id="wind" />
+              <h3 id="humidity" />
+            </div>
+            <div className="temperature" id="temperature">
+              <h1 className="temp" id="temp">
+                <i id="condition" /> <span id="num" />
+                <a className="fahrenheit active" id="fahrenheit" href="#">
+                  °F
+                </a>
+                <span className="divider secondary">|</span>
+                <a className="celsius" id="celsius" href="#">
+                  °C
+                </a>
+              </h1>
+            </div>
+            <div className="forecast" id="forecast" />
+          </div>
+        </div>
       </div>
     </div>
-    <div className="card">
-      <div className="card-image">
-        <img src="https://assets.codepen.io/4787486/calligraphy-dragon.jpg" />
-      </div>
-      <div className="card-text">
-        <h2>Calligrapher</h2>
-        <p>
-          Nori grape silver beet broccoli kombu beet greens fava bean potato
-          quandong celery.
-        </p>
-      </div>
-      <div className="card-inner">
-        <button>Read More</button>
-        <i className="fa fa-thumbs-o-up fa-lg" aria-hidden="true" />
-      </div>
-    </div>
-    <div className="card">
-      <div className="card-image">
-        <img src="https://assets.codepen.io/4787486/calligraphy-wits.jpg" />
-      </div>
-      <div className="card-text">
-        <h2>Famous Quotes</h2>
-        <p>
-          Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat
-          tbh semiotics artisan synth stumptown gastropub cornhole celiac swag.
-        </p>
-      </div>
-      <div className="card-inner">
-        <button>Read More</button>
-        <i className="fa fa-thumbs-o-up fa-lg" aria-hidden="true" />
-      </div>
-    </div>
-  </section>
-</main>
+    <div className="content-wrapper"></div>
+  </div>
+</>
+
 
     );
   }
