26b72e83bf3053813163fc60a35ad22a96b632d6
Vishnu Raja <323993+techbillies@users.noreply.github.com>
2023-10-09 23:39:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,34 +8,7 @@ class App extends Component {
     return (
      
      <>
-  {/* <input type="text" id="search-input" placeholder="Search...">*/}
-  <select id="category-filter" className="custom-dropdown">
-    <option value="all"> /All</option>
-    {/* Add more options based on extracted categories */}
-  </select>{" "}
-  <div className="content-item" data-category="Local">
-    <div>
-      <div className="wrapper">
-        <div
-          className="kads1"
-          style={{
-            float: "left",
-            position: "absolute",
-            left: "50%",
-            marginLeft: "-364px",
-            top: 166,
-            maxWidth: 728,
-            width: "100%",
-            height: 90,
-            border: "1px solid #bcbcbc69",
-            textAlign: "center",
-            lineHeight: "5.59",
-            fontFamily: "arial",
-            color: "#799bc6"
-          }}
-        >
-          Advertise
-        </div>
+ 
         <div className="panel">
           <h2 className="city" id="city" />
           <div className="weather" id="weather">
